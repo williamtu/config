f9b55bdd2e6906472611fc739bdd0c553fe0417b
William Tu <u9012063@gmail.com>
2017-08-19 06:24:00 -0700

    add build kernel script

---
+++ build-kernel.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# $#: number of args passed
+
+# enable sparse checking
+CF="-Wsparse-all -D__CHECKER__ -D__CHECK_ENDIAN__ -Wbitwise"
+LOCALVERSION=$(git branch | sed -n -e "s/* \(.*\)/\1/p" | sed -e "s/_/-/g")
+TARGET=bindeb-pkg
+
+if [ $# -gt 1 ]; then
+	echo "usage: %0 [localversion]"
+	exit 1
+elif [ $# -eq 1 ]; then
+	LOCALVERSION=$1
+fi
+
+# create new branch
+git checkout -b $LOCALVERSION
+make olddefconfig
+
+# check 
+NPROC=$(getconf _NPROCESSORS_ONLN)
+NPROC1=$(($NPROC - 1))
+make -j"$NPROC" C=1 CF="$CF" LOCALVERSION="-$LOCALVERSION" $TARGET
+
