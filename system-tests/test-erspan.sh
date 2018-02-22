f5aba73da701bcf5cf25bb6bc6ac0a46e182ea86
William Tu <u9012063@gmail.com>
2018-02-22 13:26:43 -0800

    add gdb xterm

--- system-tests/test-erspan.sh
+++ system-tests/test-erspan.sh
@@ -3,6 +3,9 @@
 
 trap cleanup 0 2 3 9
 
+#rmmod openvswitch
+#modprobe openvswitch
+
 rm -f /usr/local/etc/openvswitch/conf.db
 ovsdb-tool create /usr/local/etc/openvswitch/conf.db /root/ovs/vswitchd/vswitch.ovsschema
 
@@ -11,7 +14,7 @@ ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
     --pidfile --detach
 
 ovs-vsctl --no-wait init 
-ovs-vswitchd  --detach --no-chdir --pidfile --log-file -vvconn -vofproto_dpif -vunixctl
+ovs-vswitchd  --detach --no-chdir --pidfile --log-file=/root/ovs/ovs-vswitchd.log -vvconn -vofproto_dpif -vunixctl
 
 ovs-vsctl -- add-br br0 -- set Bridge br0 protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13,OpenFlow14,OpenFlow15 fail-mode=secure 
 ovs-vsctl -- add-br br-underlay -- set Bridge br0 protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13,OpenFlow14,OpenFlow15 fail-mode=secure
@@ -37,14 +40,14 @@ ip addr add dev br-underlay "172.31.1.100/24"
 ip link set dev br-underlay up
 ovs-vsctl add-port br0 at_erspan0 -- \
               set int at_erspan0 type=erspan options:key=1 options:remote_ip=172.31.1.1 \
-              options:erspan_ver=1 options:erspan_idx=7 
+              options:erspan_ver=1 options:erspan_idx=6 
 
 ip addr add dev br0 10.1.1.100/24
 ip link set dev br0 up
 ip link set dev br0 mtu 1450
 
 ip netns exec at_ns0 sh << NS_EXEC_HEREDOC
-ip link add dev ns_erspan0 type erspan seq key 1 erspan_ver 1 erspan 7 remote 172.31.1.100
+ip link add dev ns_erspan0 type erspan seq key 1 erspan_ver 1 erspan 1 remote 172.31.1.100
 ip addr add dev ns_erspan0 10.1.1.1/24
 ip link set dev ns_erspan0 mtu 1450  up
 NS_EXEC_HEREDOC
