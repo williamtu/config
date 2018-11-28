fabc238ceb9a18472bafd4ae72dee7d19c300f30
William Tu <u9012063@gmail.com>
2018-11-28 14:27:25 -0800

    add vxlan user test

--- script/ip6gre.sh
+++ script/ip6gre.sh
@@ -69,7 +69,7 @@ main
 ping6 -c 3 fc00:100::1
 
 # Ping overlay network from NS0
-ping6 -c 3 fc00:200::1 
+ping6 -c 300 fc00:200::1 
 ping -c 3 10.10.200.1
 
 # ip -6 route show
--- system-tests/test-vxlan-user.sh
+++ system-tests/test-vxlan-user.sh
@@ -14,11 +14,10 @@ ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
     --pidfile --detach
 
 ovs-vsctl --no-wait init 
-ovs-vswitchd  --detach --no-chdir --pidfile --log-file=/root/ovs/ovs-vswitchd.log -vvconn -vofproto_dpif -vunixctl
-#--disable-system
+ovs-vswitchd --disable-system --detach --no-chdir --pidfile --log-file=/root/ovs/ovs-vswitchd.log -vvconn -vofproto_dpif -vunixctl
 
-ovs-vsctl -- add-br br0 -- set Bridge br0 protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13,OpenFlow14,OpenFlow15 fail-mode=secure datapath_type=netdev 
-ovs-vsctl -- add-br br-underlay -- set Bridge br-underlay protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13,OpenFlow14,OpenFlow15 fail-mode=secure datapath_type=netdev
+ovs-vsctl add-br br0 -- set Bridge br0 datapath_type="netdev" protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13,OpenFlow14,OpenFlow15 fail-mode=secure
+ovs-vsctl -- add-br br-underlay -- set Bridge br-underlay protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13,OpenFlow14,OpenFlow15 fail-mode=secure datapath_type="netdev"
 
 ovs-ofctl add-flow br0 "actions=normal"
 ovs-ofctl add-flow br-underlay "actions=normal"
@@ -27,12 +26,15 @@ ovs-appctl vlog/set dbg
 
 ip netns add at_ns0
 ip link add p0 type veth peer name ovs-p0
+ethtool -K p0 tx off
+ethtool -K p0 rxvlan off
+ethtool -K p0 txvlan off
+
 ip link set p0 netns at_ns0
 ip link set dev ovs-p0 up
-ethtool -K p0 tx off
 
 ovs-vsctl add-port br-underlay ovs-p0 -- \
-                set interface ovs-p0 external-ids:iface-id="p0"
+                set interface ovs-p0 external-ids:iface-id="p0" type="afxdp"
 
 ip netns exec at_ns0 sh << NS_EXEC_HEREDOC
 ip addr add "172.31.1.1/24" dev p0
@@ -42,23 +44,28 @@ NS_EXEC_HEREDOC
 
 ip addr add dev br-underlay "172.31.1.100/24"
 ip link set dev br-underlay up
-ovs-vsctl add-port br0 at_gre0 -- \
-              set int at_gre0 type=gre options:remote_ip=172.31.1.1
+ovs-vsctl add-port br0 at_vxlan0 -- \
+              set int at_vxlan0 type=vxlan options:remote_ip=172.31.1.1 
 
 ip addr add dev br0 10.1.1.100/24
 ip link set dev br0 up
-ip link set dev br0 address 00:51:16:1e:14:1f 
 ip link set dev br0 mtu 1450
 
-#ovs-appctl ovs/route/add 10.1.1.100/24 br0
+ovs-appctl ovs/route/add 10.1.1.100/24 br0
+ovs-appctl ovs/route/add 172.31.1.92/24 br-underlay
+ovs-appctl ovs/route/show
+
 
 #underlay
 ping -c 2 172.31.1.1
 
 ip netns exec at_ns0 sh << NS_EXEC_HEREDOC
-ip link add dev ns_gretap0 type gretap remote 172.31.1.100
-ip addr add dev ns_gretap0 10.1.1.1/24
-ip link set dev ns_gretap0 mtu 1450  up
+ip link add dev at_vxlan1 type vxlan remote 172.31.1.100 id 0 dstport 4789
+ip addr add dev at_vxlan1 10.1.1.1/24
+ip link set dev at_vxlan1 mtu 1450  up
+ping -q -c 3 -i 0.3 -w 2 172.31.1.100 | grep "transmitted" | sed 's/time.*ms$/time 0ms/'
+ping -q -c 3 -i 0.3 -w 2 10.1.1.100 | grep "transmitted" | sed 's/time.*ms$/time 0ms/'
+ping -s 1600 -q -c 3 -i 0.3 -w 2 10.1.1.100 | grep "transmitted" | sed 's/time.*ms$/time 0ms/'
 NS_EXEC_HEREDOC
 
 ip netns exec at_ns0 sh << NS_EXEC_HEREDOC
